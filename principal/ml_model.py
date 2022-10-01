909d70d1ef984fc28be3c447d0fc09f8cfd2fd23
PGuillenOrtiz <guillenpablo@gmail.com>
2022-10-01 17:49:41 +0200

    Update ml_model.py

--- principal/ml_model.py
+++ principal/ml_model.py
@@ -1,18 +1,29 @@
 import pickle
 import itertools
+from .models import Principal
+from django.shortcuts import render
+from django.templatetags.static import static
+from django.conf import settings
+import os
+
+def open_model(file_name):
+    models_folder = settings.BASE_DIR
+    file_path = os.path.join(models_folder, os.path.basename(file_name))
+    model = pickle.load(open(file_path, "rb"))
+    return model
 
 class ml_heartAttack:
     def __init__(self, f_datos):
         self.f_datos = f_datos
 
     def t_result(self):
-        with open('D:\ClasesPython\Django\predictML\principal\modelforrest_pickle','rb') as f:
-            mod = pickle.load(f)
-        with open('D:\ClasesPython\Django\predictML\principal\model_scaler','rb') as f:
-            scaler = pickle.load(f)
+        bb = (static('img/modelforrest_pickle'), 'rb')
+        #with open(bb,'rb') as f:
+        mod = open_model('modelforrest_pickle')
+        #with open(static('/img/model_scaler'),'rb') as f:
+        scaler = open_model('model_scaler')
         valores_transform = scaler.transform([self.f_datos])
         t_result = mod.predict_proba(valores_transform)
         t_result = list(itertools.chain.from_iterable(t_result))
         t_result = [i*100 for i in t_result]
-        #t_result = t_result[0,1]
         return t_result
